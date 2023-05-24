a2b65bfe7b62e58f6ea8e8ae57f2cb207eaf263e
Jessyjy7 <jiy018@ucsd.edu>
2023-05-24 16:40:36 -0700

    changes made

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.:grading-area/hamcrest-core-1.3.jar:grading-area/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -7,6 +7,21 @@ mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
+
+if [[ -e student-submission/ListExamples.java ]]
+then 
+    echo 'fill found'
+else
+    echo 'fill not found'
+fi
+
+cp TestListExamples.java grading-area
+cp student-submission/ListExamples.java grading-area
+cp lib/hamcrest-core-1.3.jar grading-area
+cp lib/junit-4.13.2.jar grading-area
+
+cd grading-area/
+javac -cp $CPATH ListExamples.java TestListExamples.java
 
 
 # Draw a picture/take notes on the directory structure that's set up after
